906b7aeb18abaf0b69225d83e886b3c4c3aaf16b
richdanis <da.richard@gmx.de>
2026-03-28 10:37:40 +0100

    First working matrix multiplication!

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,18 @@ use rand::distr::Uniform;
 use rand::prelude::*;
 
 fn main() {
-    let rand_vec = random_vector(0.0, 1.0, 6);
-    let empty_linear = Linear::new(3, 2);
+    let d_in = 3;
+    let d_out = 2;
+    let weights = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+    //let weights = random_vector(0.0, 1.0, d_in * d_out);
+    let linear = Linear {
+        d_in,
+        d_out,
+        weights,
+    };
+    let input = vec![1.0, 1.0, 1.0];
+    let output = linear.forward(&input);
+    println!("{:?}", output);
 }
 
 fn random_vector(low: f32, high: f32, size: usize) -> Vec<f32> {
@@ -19,13 +29,21 @@ struct Linear {
 }
 
 impl Linear {
-    fn new(d_in: usize, d_out: usize) -> Linear {
-        let weights = Vec::with_capacity(d_in * d_out);
-        Linear {
-            d_in,
-            d_out,
-            weights,
+    fn forward(&self, x: &[f32]) -> Vec<f32> {
+        // Ensure same dimension
+        assert!(
+            x.len() == self.d_in,
+            "Input vector has dim {} incompatible with linear layer input dim {}.",
+            x.len(),
+            self.d_in
+        );
+
+        let mut output: Vec<f32> = Vec::with_capacity(self.d_out);
+        for row in 0..self.d_out {
+            for col in 0..self.d_in {
+                output.push(x[col] * self.weights[row * self.d_in + col]);
+            }
         }
+        output
     }
-    fn forward(&self) {}
 }
